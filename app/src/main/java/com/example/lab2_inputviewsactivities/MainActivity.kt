a3d4ed0d25111a2ba79a0b398c25a1d5d3819156
sarahkurd <skurdogh@calpoly.edu>
2018-10-07 16:30:09 -0700

    UI buttons/XML completed

--- app/src/main/java/com/example/lab2_inputviewsactivities/MainActivity.kt
+++ app/src/main/java/com/example/lab2_inputviewsactivities/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.lab2_inputviewsactivities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +12,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun onRadioButtonClicked(view: View) {
+        if (view is RadioButton) {
+            // Is the button now checked?
+            val checked = view.isChecked
+
+            // Check which radio button was clicked
+            when (view.getId()) {
+                R.id.radioButton1 ->
+                    if (checked) {
+                        textView.text = "True"
+                    }
+                R.id.radioButton2 ->
+                if (checked) {
+                    textView.text = "False"
+                }
+                R.id.radioButton3 ->
+                    if (checked) {
+                        textView.text = "Get it together"
+                    }
+            }
+        }
+    }
+
 }
